153bc3ba7ffa1492f84047ef11d14373c3a8bfda
Carloslora5 <carloslora123@outlook.com>
2026-03-02 21:32:06 -0600

    first commit spring boot employee service

---
+++ src/main/java/com/invex/employee/InvexEmployeeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.invex.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvexEmployeeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvexEmployeeServiceApplication.class, args);
+	}
+
+}
